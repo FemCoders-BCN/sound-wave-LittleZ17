993bd4710796cdad31a4c2ae996c201d220c4893
Little Z <zy_mz17@hotmail.es>
2023-01-03 17:07:22 +0100

    stucture ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import {Routes, Route} from 'reac-router-dom'
+import {Routes, Route} from 'react-router-dom'
+import Home from './Home/Home'
+import Discover from './Discover/Discover'
+import Join from './Join/Join'
+
 
 
 function App() {
@@ -6,9 +10,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element='<Home />'/>
-        <Route path='discover' element='<Discover />'/>
-        <Route path='join' element='<Join />'/>
+        <Route path='/' element={<Home />}/>
+        <Route path='discover' element={<Discover />}/>
+        <Route path='join' element={<Join />}/>
       </Routes>
     </>
   )
--- src/Join/Join.jsx
+++ src/Join/Join.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 const Join = () => {
   return (
-    <div>Join</div>
+    <div>Join!!</div>
   )
 }
 
